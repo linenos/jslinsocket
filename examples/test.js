75eafbbcc711868d95e92e655a8dafd87cd840ed
Linen <linenreal@gmail.com>
2024-02-17 22:47:00 -0500

    Finished

---
+++ examples/test.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const route = express();
+const Linsocket = require(".././index");
+const server = new Linsocket.Server(route, { path: "/customws" });
+
+// Visit the golang client: https://github.com/linenos/golinsocket
+
+server.on("connect", function(ws) {
+    /*
+    ws -> {
+        emit: function(event: string, ...arguments),
+        on: function(event: string, callback: function)
+        headers: object {
+            "host": "host will be here"
+            ...
+        }
+    }
+    */
+
+    // Client (golang): server.Emit("login", "example_username", "example_password", "example_otherthing")
+    ws.on("login", (username, password, otherthing) => {
+        console.log(username) // -> "example_username" / string
+        console.log(password) // -> "example_password" / string
+        console.log(otherthing) // -> "example_otherthing" / string
+    })
+
+    // Check out the golang client: 
+    ws.emit("hello", "buirehbgjieruhbgne", "haha", "brrr", "inf arguments", "do NOT send functions in the argument")
+})
+
+route.listen(4000, () => {
+    console.log("Server Listening on port " + 4000)
+})
---
+++ readme.md
@@ -0,0 +1,7 @@
+linsocket
+
+Websocket clone of socket.io written in Node.js
+Client that works with linsocket: https://github.com/linenos/golinsocket
+
+Contact me to suggest features or to fix bugs ( ON DISCORD ): Linen#3485
+Look at the examples/test.js file for example usage
